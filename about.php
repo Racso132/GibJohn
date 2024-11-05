b2761c7c5eb000cee482b002bdeb150a7e7e5a36
Oscar Fuggle <338086@edu.mkcollege.ac.uk>
2024-11-05 10:15:34 +0000

    Add about.php page with company information and structure

---
+++ about.php
@@ -0,0 +1,30 @@
+<?php
+session_start()
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us - online tutoring </title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <?php include './parts/header.php'; ?> 
+    
+    <section>
+        <h2>About Us</h2>
+        <p>We are a company who loves to take care of peoples learning and timetable plans we offer a variety of plans you can use for your specific needs.</p>
+        <p>you can create a better you</p>
+        <p>Our company is based in the United Kingdom and we have a team of experts who are ready to help you with your learning and subject needs.</p>
+        <p>One of the top recomended </p>    <a href=""></a>
+        <p>One of the top recomended </p>    <a href=""></a>
+        <p>use of css later(personal use only)</p>    <a href="https://www.w3schools.com/css/css3_buttons.asp">css use</a>
+    </section>
+    <?php include './parts/footer.php'; ?>
+    
+
+</body>
+</html>
+  
