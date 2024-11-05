9bf71a427a62db133c08257a47c217a10b54a21b
Oscar Fuggle <338086@edu.mkcollege.ac.uk>
2024-11-05 14:38:10 +0000

    Replace styles.css with style.css and update styling for improved layout

--- about.php
+++ about.php
@@ -8,7 +8,7 @@ session_start()
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Us - online tutoring </title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <?php include './parts/header.php'; ?> 
