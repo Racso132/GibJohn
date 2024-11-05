fdfc513d31dbf3f9cb69d82304ea62afff469708
Oscar Fuggle <338086@edu.mkcollege.ac.uk>
2024-11-05 09:50:42 +0000

    add index.php file

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>workout routine's</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    
+    <?php include './parts/header.php'; ?>
+    
+    <section>
+        <h2>Welcome to the home page for your daily workout</h2>
+        <br>Explore the top rated routine's and plans for the best prices</br>
+        In this website youll be able to look at a variety of workout routines for your needs.</br>
+        throughout this website youll be able to find a variety of workout plans for your needs.</br>
+        also you should find some helpful dietry plans and healthy eating plans these will help keep your energy up and making you look fitter and healthier.</br>
+        Our company was founded in 2024 and we have been helping people with their workout plans ever since.</br>
+
+    
+    </section>
+    
+    <?php include './parts/footer.php'; ?>
+
+</body>
+</html>  
