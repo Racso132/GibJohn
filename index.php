16190c2263f3231e6aae0ef9eee8be7741c751a9
Oscar Fuggle <338086@edu.mkcollege.ac.uk>
2024-11-05 10:13:01 +0000

    Update index.php to reflect online tutoring theme and content

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>workout routine's</title>
+    <title> Online Tutoring </title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
@@ -11,12 +11,11 @@
     <?php include './parts/header.php'; ?>
     
     <section>
-        <h2>Welcome to the home page for your daily workout</h2>
-        <br>Explore the top rated routine's and plans for the best prices</br>
-        In this website youll be able to look at a variety of workout routines for your needs.</br>
-        throughout this website youll be able to find a variety of workout plans for your needs.</br>
-        also you should find some helpful dietry plans and healthy eating plans these will help keep your energy up and making you look fitter and healthier.</br>
-        Our company was founded in 2024 and we have been helping people with their workout plans ever since.</br>
+        <h2>Welcome to the home page for your online tutoring </h2>
+        <br>Explore the top rated tutors and plans for the best prices</br>
+        In this website youll be able to look at a variety of revision plans and subjects for your needs.</br>
+        also you should find some helpful revision plans and subject timetable plans these will help keep your mind working and still give you time to enjoy learning.</br>
+        Our company was founded in 2024 and we have been helping people with their learning ever since.</br>
 
     
     </section>
