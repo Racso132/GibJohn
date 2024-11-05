4824c3d0b314ada1d3614073d07b2171905e0fbc
Oscar Fuggle <338086@edu.mkcollege.ac.uk>
2024-11-05 14:21:54 +0000

    Add header section with navigation links for site structure

---
+++ parts/header.php
@@ -0,0 +1,20 @@
+<header>
+    <h1>GIBJOHN</h1>
+    <h2>  Online tutoring </h2>
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="about.php">About</a>
+        <a href="contact.php">Contact</a>   
+        <a href="termsandconditions.php">terms and condition</a>
+        
+        <?php if (isset($_SESSION['user']['id'])): ?>
+            <a href="dashboard.php">Dashboard</a>
+            <a href="logout.php">Logout</a>
+            <a href=""></a>
+            <a href="paidhealthplan.php">paid plan</a>
+        <?php else: ?>
+            <a href="register.3">Register</a>
+            <a href="login.php">Login</a>
+        <?php endif; ?>
+    </nav> 
+</header>
