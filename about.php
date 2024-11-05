52acaffd4acb873527bca6b7ab0c654b7206f6dd
Oscar Fuggle <338086@edu.mkcollege.ac.uk>
2024-11-05 12:30:13 +0000

    Update about page content and add comprehensive styling to enhance site layout

--- about.php
+++ about.php
@@ -18,7 +18,7 @@ session_start()
         <p>We are a company who loves to take care of peoples learning and timetable plans we offer a variety of plans you can use for your specific needs.</p>
         <p>you can create a better you</p>
         <p>Our company is based in the United Kingdom and we have a team of experts who are ready to help you with your learning and subject needs.</p>
-        <p>One of the top recomended </p>    <a href=""></a>
+        <p>Another website that you can go to for more information and revision </p>    <a href=""></a>
         <p>One of the top recomended </p>    <a href=""></a>
         <p>use of css later(personal use only)</p>    <a href="https://www.w3schools.com/css/css3_buttons.asp">css use</a>
     </section>
