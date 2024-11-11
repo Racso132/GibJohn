a7de3c6208f8bdfe1766b09a5e1db4813248631c
Oscar Fuggle <338086@edu.mkcollege.ac.uk>
2024-11-11 11:22:43 +0000

    changed style and created new database for now

--- includes/db.php
+++ includes/db.php
@@ -3,5 +3,14 @@ $dsn = 'mysql:host=localhost:3306;dbname=test';
 $db_username = 'root';
 $db_password = '';
 
-$pdo = new PDO($dsn, $db_username, $db_password);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
+// $pdo = new PDO($dsn, $db_username, $db_password);
+// $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
+
+// Dummy PDO object to avoid error
+$pdo = new stdClass();
+$pdo->setAttribute = function() {};
+$pdo->prepare = function() {};
+$pdo->execute = function() {};
+$pdo->fetch = function() {};
+$pdo->fetchAll = function() {};
+$pdo->lastInsertId = function() {};
