d8aa46b0d1cf1b58423df17e689c41e3e14f10f1
Racso132 <338086@edu.mkcollege.ac.uk>
2024-11-11 12:05:16 +0000

    Update database connection details in db.php and remove dummy PDO object

--- includes/db.php
+++ includes/db.php
@@ -1,16 +1,16 @@
 <?php
-$dsn = 'mysql:host=localhost:3306;dbname=test';
+$dsn = 'mysql:host=localhost:3306;dbname=users';
 $db_username = 'root';
 $db_password = '';
 
-// $pdo = new PDO($dsn, $db_username, $db_password);
-// $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
+$pdo = new PDO($dsn, $db_username, $db_password);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
 
 // Dummy PDO object to avoid error
-$pdo = new stdClass();
-$pdo->setAttribute = function() {};
-$pdo->prepare = function() {};
-$pdo->execute = function() {};
-$pdo->fetch = function() {};
-$pdo->fetchAll = function() {};
-$pdo->lastInsertId = function() {};
+// $pdo = new stdClass();
+//$pdo->setAttribute = function() {};
+//$pdo->prepare = function() {};
+//$pdo->execute = function() {};
+//$pdo->fetch = function() {};
+//$pdo->fetchAll = function() {};
+//$pdo->lastInsertId = function() {};
