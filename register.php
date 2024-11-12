d18a47aec937ef20af302c106da3d1436ba2d9e5
Oscar Fuggle <338086@edu.mkcollege.ac.uk>
2024-11-12 12:09:55 +0000

    Refactor header structure by including external header file and adjust styling with border color and margin

--- register.php
+++ register.php
@@ -34,19 +34,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <body>
     <header>
         <h1>GibJohn</h1>
-        <nav>
-            <a href="index.php">Home</a>
-            <a href="about.php">About</a> 
-            <a href="contact.php">Contact</a>
-        
-            <?php if (isset($_SESSION['user']['id'])): ?>
-                <a href="dashboard.php">Dashboard</a>
-                <a href="logout.php">Logout</a>
-            <?php else: ?>
-                <a href="register.php">Register</a>
-                <a href="login.php">Login</a>
-            <?php endif; ?>
-        </nav>
+        <?php include './parts/header.php'; ?> 
         
     </header>
 
