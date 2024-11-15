1d558b6170acd9183ac15bd344242f546b9126d4
Oscar Fuggle <338086@edu.mkcollege.ac.uk>
2024-11-15 12:02:43 +0000

    Enhance header navigation by bolding menu items for improved visibility

--- parts/header.php
+++ parts/header.php
@@ -2,19 +2,19 @@
     <h1>GIBJOHN</h1>
     <h2>  Online tutoring </h2>
     <nav> 
-        <a href="index.php">Home</a>
-        <a href="about.php">About</a>
-        <a href="contact.php">Contact</a>   
-        <a href="termsandconditions.php">terms and condition</a>
-        <a href="accessibility.php">Accessibility</a>
+        <b><a href="index.php">Home</a></b>
+        <b><a href="about.php">About</a></b>
+        <b><a href="contact.php">Contact</a></b>   
+        <b><a href="termsandconditions.php">terms and condition</a></b>
+        <b><a href="accessibility.php">Accessibility</a></b>
         
         <?php if (isset($_SESSION['user']['id'])): ?>
-            <a href="packages.php">Packages</a>
-            <a href="dashboard.php">Dashboard</a>
-            <a href="logout.php">Logout</a>
+            <b><a href="packages.php">Packages</a></b>
+            <b><a href="dashboard.php">Dashboard</a></b>
+            <b><a href="logout.php">Logout</a></b>
         <?php else: ?>
-            <a href="register.php">Register</a>
-            <a href="login.php">Login</a>
+            <b><a href="register.php">Register</a></b>
+            <b><a href="login.php">Login</a></b>
         <?php endif; ?>
     </nav> 
 </header> 
