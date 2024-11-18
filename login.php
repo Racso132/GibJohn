72c62989fb2849cd2a3dc3df31819ab8044f802f
Racso132 <338086@edu.mkcollege.ac.uk>
2024-11-18 17:14:50 +0000

    Enhance login functionality by adding email input; update SQL query to include email and username for user verification, and improve error message clarity.

--- login.php
+++ login.php
@@ -6,24 +6,25 @@ include './includes/db.php';
 // Check if the form has been submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     try {
-        // Retrieve the username and password from the POST request
-        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+        // Retrieve the email, username, and password from the POST request
+        $email = $_POST['email'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
 
-        // Prepare the SQL statement for inserting the user into the database
-        $stmt = $pdo->prepare("SELECT * FROM users WHERE username=?");
+        // Prepare the SQL statement for selecting the user from the database
+        $stmt = $pdo->prepare("SELECT * FROM users WHERE email=? AND username=?");
         // Execute the statement
-        $stmt->execute([$_POST['username']]);
+        $stmt->execute([$email, $username]);
         $usr = $stmt->fetch();
 
-        if (password_verify($_POST['password'], $usr['password'])) {
+        if ($usr && password_verify($password, $usr['password'])) {
             $_SESSION['user']['id'] = $usr['id'];
             $_SESSION['user']['username'] = $usr['username']; 
             header('Location: dashboard.php');
         } else {
-            $error = 'uh oh something went wrong please make sure you have registered first or made sure your username and/or password are correct if  this proceeds to happen again and you have done the above please contact us or try re-registering or try again later. if you have forgotten your password please contact us and we will help you reset it';
+            $error = 'Uh oh, something went wrong. Please make sure you have registered first or made sure your email, username, and/or password are correct. If this issue persists, please contact us or try re-registering or try again later. If you have forgotten your password, please contact us and we will help you reset it.';
         } 
 
-        // echo "Registration successful!";
     } catch (PDOException $e) {
         echo "Error: " . $e->getMessage();
     }
@@ -38,6 +39,9 @@ new part_header("Login");
     <div>
         <form action="login.php" method="post">
             <?php if (isset($error)) echo $error; ?>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+
             <label for="username">Username:</label>
             <input type="text" id="username" name="username" required>
 
