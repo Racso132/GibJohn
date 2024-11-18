c9eff482848a9f134fd10e98c43749046672917c
Oscar Fuggle <338086@edu.mkcollege.ac.uk>
2024-11-18 09:50:36 +0000

    Refactor footer and header links for consistency and clarity; update CSS for proper navigation width

--- parts/footer.php
+++ parts/footer.php
@@ -3,7 +3,6 @@
             <p>all rights reserved for my company GibJohn</p>
     <p>Any use of the GibJohn trademark will have a letter through their door with a court date</p> 
     <p>For any questions or issues, please <a href="contact.php">contact us</a>.</p>
-    <p>Follow us on <a href="https://www.facebook.com/">Facebook</a>, <a href="https://twitter.com/">Twitter</a>, and <a href="https://www.instagram.com/">Instagram</a>.</p>
     <p>For more information, see our <a href="termsandcondition.php">terms of service</a>.</p>
     <img src="" alt="">
 </footer>
--- parts/header.php
+++ parts/header.php
@@ -5,7 +5,7 @@
         <a href="index.php">Home</a>
         <a href="about.php">About</a>
         <a href="contact.php">Contact</a>
-        <a href="termsandconditions.php">terms and condition</a>
+        <a href="termsandcondition.php">terms and condition</a>
         <a href="accessibility.php">Accessibility</a>
         
         <?php if (isset($_SESSION['user']['id'])): ?>
