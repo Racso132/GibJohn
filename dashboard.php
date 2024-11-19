db572797eb8fecf464cc9a761672a5d1b37c0ac0
OscarF <338086@edu.mkcollege.ac.uk>
2024-11-19 14:00:02 +0000

    Add learning_together image with styling to dashboard.php and style.css

--- dashboard.php
+++ dashboard.php
@@ -16,9 +16,14 @@ new part_header("Dashboard");
 
 <section>
     <h2>Hello, <?php echo htmlspecialchars($_SESSION['user']['username']); ?>!</h2> 
+    <img class="learning_together" src="learning_together.png">
+        <h3>Learning Together</h3>
+        <p>Our mission is to help you with your learning and revision needs. We aim to provide you with the best resources and tools to help you succeed in your studies.</p>
+        <p>Our team of experts are dedicated to helping you achieve your goals and reach your full potential.</p>
+        <p>We are committed to providing you with the best possible service and support to help you succeed in your studies.</p>
     <p>This is a protected page. Only logged-in users can access this dashboard.</p>
     <p>Feel free to explore exclusive content or manage your account here.</p>
     <p>For any questions or issues, please <a href="contact.php">contact us</a>.</p>
-    <img src=""> 
 </section>
+</div>
 <?php include './parts/footer.php'; ?>
