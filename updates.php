52b43328ed70e937882f2814747ec0e806f6cde7
OscarF <338086@edu.mkcollege.ac.uk>
2024-11-19 11:21:33 +0000

    Enhance responsive design in style.css and update updates.php to include confirmation of password field addition

--- updates.php
+++ updates.php
@@ -1,1 +1,4 @@
 <p>we have resolved the login and register issue. thank you for your patience</p>
+<p> we have also added a confirm password field in the register page</p>
+<p> 
+<?php include './parts/footer.php'; ?>
